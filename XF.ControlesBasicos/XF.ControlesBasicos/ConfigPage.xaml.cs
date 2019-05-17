c586443aa114d2687bd7ff858088306cf2261ede
beirinha <beirinha@gmail.com>
2019-05-17 01:17:36 -0300

    Adicionar arquivos de projeto.

---
+++ XF.ControlesBasicos/XF.ControlesBasicos/ConfigPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XF.ControlesBasicos
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ConfigPage : ContentPage
+    {
+        public ConfigPage()
+        {
+            InitializeComponent();
+
+            if (Application.Current.Properties.ContainsKey("email"))
+            {
+                string email = Application.Current.Properties["email"] as string;
+                swEnviar.On = true;
+                txtEmail.IsVisible = true;
+                txtEmail.Text = email;
+            }            
+        }
+
+        private void BtnVoltar_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PopAsync();
+        }
+
+        private void SwEnviar_OnChanged(object sender, ToggledEventArgs e)
+        {
+            txtEmail.IsVisible = swEnviar.On;
+        }
+
+        private void ContentPage_Disappearing(object sender, EventArgs e)
+        {
+            if (!swEnviar.On || string.IsNullOrEmpty(txtEmail.Text))
+                Application.Current.Properties.Clear();
+            else
+                Application.Current.Properties["email"] = txtEmail.Text;
+        }
+    }
+}
---
+++ XF.ControlesBasicos/XF.ControlesBasicos/MainPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XF.ControlesBasicos
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(true)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnEnviarEmail_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                if (Application.Current.Properties.ContainsKey("email"))
+                {
+                    string email = Application.Current.Properties["email"] as string;
+                    DisplayAlert("Mensagem", "E-mail enviado para " + email.ToString(), "OK");
+                }
+                else
+                {
+                    DisplayAlert("Mensagem", "Nenhum E-mail configurado", "OK");
+                }
+            }
+            catch (Exception ex)
+            {
+                DisplayAlert("Erro", ex.Message, "OK");
+            }
+        }
+
+        private void BtnConfig_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new ConfigPage());
+        }
+    }
+}
